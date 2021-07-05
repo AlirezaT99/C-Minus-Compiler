3062d711fbc7ee1e7026929db05aa18d231ba07d
erfanfi <erfanfaravani@gmail.com>
2021-07-05 10:40:24 +0430

    semantic analyser part void,scope,break done

--- codegen.py
+++ codegen.py
@@ -1,14 +1,24 @@
 import utils
 
 
+def search_in_symbol_table(item, scope_num=0):
+    for record in utils.symbol_table['ids'][::-1]:
+        if item == record[0] and record[3] <= scope_num:
+            return record[2]
+    return False
+
+
 class CodeGenerator:
     def __init__(self):
         self.SS = list()
         self.PB = dict()
         self.break_stack = list()
+        self.current_scope = 0
         self.index = 0
         self.temp_address = 500
         self.operations_dict = {'+': 'ADD', '-': 'SUB', '<': 'LT', '==': 'EQ'}
+
+        self.id_type = 'void'
 
     @staticmethod
     def find_address(item):
@@ -19,6 +29,9 @@ class CodeGenerator:
                 return record[2]
 
     def call_routine(self, name, lookahead):
+        # if len(self.PB)>1:
+        #     print(name,lookahead)
+        #     print(self.PB)
         self.__getattribute__(name[1:])(lookahead)
 
     def insert_code(self, part1, part2, part3='', part4=''):
@@ -34,21 +47,29 @@ class CodeGenerator:
 
     def define_variable(self, lookahead):
         var_id = self.SS.pop()
+        self.void_check(var_id)
+
         address = self.get_temp()
-        utils.symbol_table['ids'].append((var_id, 'int', address))
+        utils.symbol_table['ids'].append((var_id, 'int', address, self.current_scope))
 
     def define_array(self, lookahead):
         array_size, array_id = int(self.SS.pop()[1:]), self.SS.pop()
+        self.void_check(array_id)
+
         address = self.get_temp()
         array_space = self.get_temp(array_size)
 
         self.insert_code('ASSIGN', f'#{array_space}', address)
-        utils.symbol_table['ids'].append((array_id, 'int*', address))
+        utils.symbol_table['ids'].append((array_id, 'int*', address, self.current_scope))
+
+    def get_id_type(self, lookahead):
+        self.id_type = lookahead
 
     def push_id(self, lookahead):
         self.SS.append(lookahead[2])
 
     def push_id_address(self, lookahead):
+        self.scope_check(lookahead)
         self.SS.append(CodeGenerator.find_address(lookahead[2]))
 
     def push_num(self, lookahead):
@@ -155,6 +176,7 @@ class CodeGenerator:
 
     # Break statement
     def break_loop(self, lookahead):
+        self.break_check(lookahead)
         """saves i to be later filled with a jump to after the scope"""
         self.break_stack.append(self.index)
         self.index += 1
@@ -169,3 +191,30 @@ class CodeGenerator:
         for item in self.break_stack[latest_block + 1:]:
             self.PB[item] = f'(JP, {self.index}, , )'
         self.break_stack = self.break_stack[:latest_block]
+
+    def push_scope(self, lookahead):
+        self.current_scope += 1
+
+    def pop_scope(self, lookahead):
+        for record in utils.symbol_table['ids'][::-1]:
+            if record[3] == self.current_scope:
+                del utils.symbol_table['ids'][-1]
+        self.current_scope -= 1
+
+    # semantic checks
+    def scope_check(self, lookahead):
+        if search_in_symbol_table(lookahead[2], self.current_scope):
+            return
+        utils.semantic_errors.append(f'#{lookahead[0]} : Semantic Error! {lookahead[2]} is not defined')
+
+    def void_check(self, var_id):
+        if self.id_type[2] == 'void':
+            utils.semantic_errors.append(f'#{self.id_type[0]} : Semantic Error! Illegal type of void for {var_id}')
+
+    def break_check(self, lookahead):
+        if len(self.break_stack) > 0 and ['>>>' in self.break_stack]:
+            return
+        utils.semantic_errors.append(f'#{lookahead[0]} : Semantic Error! No "while" or "switch" found for "break"')
+
+    def type_mismatch(self, lookahead):
+        pass
